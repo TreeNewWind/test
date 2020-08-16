74e0ab6d67ac9052bc4f62b5f4059b43791017c7
TreeNewWind <31409729+TreeNewWind@users.noreply.github.com>
2020-08-16 19:06:28 +0800

    test1

---
+++ 栈与括号/main.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#define MaxSize 10
+
+typedef struct{
+    char data[MaxSize];
+    int top;
+}SqStack;
+
+void InitStack(SqStack &S);
+
+
+bool StackEmpty(SqStack S);
+
+bool Push(SqStack &S, char x);
+
+bool Pop(SqStack &S, char &x);
+
+bool brackerCheck(char str[], int length)
+{
+    SqStack S;
+    for(int i = 0; i < length; i++)
+    {
+        if(str[i] == '(' || str[i] == '[' || str[i] == '{')
+            {
+
+                Push(S, str[i]);
+            }
+        else
+        {
+            if(StackEmpty(S))
+                return false;
+
+            char topElem;
+            Pop(S, topElem);
+            if(str[i] == ')' && topElem != '(')
+                return false;
+            if(str[i] == ']' && topElem != '[')
+                return false;
+            if(str[i] == '}' && topElem != '{')
+                return false;
+        }
+    }
+    return StackEmpty(S);
+}
